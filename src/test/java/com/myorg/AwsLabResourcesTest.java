cb0f86dde7c803ea0a861905edba69c435c63586
Andre  Lucas <andrelucas@Andres-MacBook-Air.local>
2023-01-16 11:25:48 +0000

    Initial commit

---
+++ src/test/java/com/myorg/AwsLabResourcesTest.java
@@ -0,0 +1,26 @@
+// package com.myorg;
+
+// import software.amazon.awscdk.App;
+// import software.amazon.awscdk.assertions.Template;
+// import java.io.IOException;
+
+// import java.util.HashMap;
+
+// import org.junit.jupiter.api.Test;
+
+// example test. To run these tests, uncomment this file, along with the
+// example resource in java/src/main/java/com/myorg/AwsLabResourcesStack.java
+// public class AwsLabResourcesTest {
+
+//     @Test
+//     public void testStack() throws IOException {
+//         App app = new App();
+//         AwsLabResourcesStack stack = new AwsLabResourcesStack(app, "test");
+
+//         Template template = Template.fromStack(stack);
+
+//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+//           put("VisibilityTimeout", 300);
+//         }});
+//     }
+// }
