4e46ee66a949269abcf8ef4982518652de305887
Andre Lucas <andrelucastic@gmail.com>
2023-01-18 17:17:09 +0000

    add action allow secretsmanager

--- src/main/java/com/andrelucastic/clusters/ecs/EcsTaskRole.java
+++ src/main/java/com/andrelucastic/clusters/ecs/EcsTaskRole.java
@@ -4,9 +4,16 @@ import com.andrelucastic.AwsResource;
 import com.andrelucastic.Environment;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.iam.Effect;
+import software.amazon.awscdk.services.iam.PolicyDocument;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.iam.Role;
 import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.constructs.Construct;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class EcsTaskRole extends Stack implements AwsResource {
 
@@ -23,6 +30,17 @@ public class EcsTaskRole extends Stack implements AwsResource {
         return Role.Builder.create(this, "ecsTaskRole")
                 .roleName(getResourceName(environment, "aws-resource-ecsTaskRole"))
                 .assumedBy(ServicePrincipal.Builder.create("ecs-tasks.amazonaws.com").build())
+                .inlinePolicies(Map.of(
+                        getResourceName(environment, "ecsTaskRolePolicy"),
+                        PolicyDocument.Builder.create()
+                                .statements(Collections.singletonList(PolicyStatement.Builder.create()
+                                        .effect(Effect.ALLOW)
+                                        .resources(Collections.singletonList("*"))
+                                        .actions(List.of(
+                                                "secretsmanager:GetSecretValue",
+                                                "secretsmanager:DescribeSecret"))
+                                        .build()))
+                                .build()))
                 .path("/")
                 .build();
     }
